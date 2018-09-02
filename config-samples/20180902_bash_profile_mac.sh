61ebc94a60cb34b63c657701992e53b0dfc53032
seiji13r <seiji13r@users.noreply.github.com>
2018-09-02 00:15:20 -0500

    Update 20180902_bash_profile_mac.sh

--- config-samples/20180902_bash_profile_mac.sh
+++ config-samples/20180902_bash_profile_mac.sh
@@ -8,7 +8,6 @@ export PATH
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-
 export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 # VIRTUALENVWRAPPER CONFIGURATION
